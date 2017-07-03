25c5d7f3ecf8beec779cddf226be1855d377b592
Ignas Jankauskas <ignasjankauskas9@gmail.com>
2017-07-03 14:45:17 +0300

    Microsoft MVC tutorial, Routing.

---
+++ FirstWebApplication/Models/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace FirstWebApplication.Models
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class PersonDBContext : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+}
