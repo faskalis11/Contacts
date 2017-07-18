04b5031a8f610a2339ff647afef4ecda75e67824
Ignas Jankauskas <ignasjankauskas9@gmail.com>
2017-07-18 12:18:26 +0300

    simple auth

---
+++ Contacts.Api/App_Start/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Facebook;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using System;
+
+[assembly: OwinStartup(typeof(Contacts.Api.App_Start.Startup))]
+
+namespace Contacts.Api.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ExternalCookie,
+                ExpireTimeSpan = new TimeSpan(0, 30, 0),
+                LoginPath = new PathString("/api/nogo")
+            });
+
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            app.UseFacebookAuthentication(new FacebookAuthenticationOptions()
+            {
+                AppId = "326718451090396",
+                AppSecret = "ccd4c4eb1a2bb944483bc88c22691bfa"
+            });
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
+            {
+                ClientId = "174311077733-dvullj1eo81jm1nni3qarmd5o27egflh.apps.googleusercontent.com",
+                ClientSecret = "zo5HHYvfLzKawwtGiFCD0m6i"
+            });
+        }
+    }
+}
---
+++ Contacts.Api/Controllers/AuthController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Contacts.Api.Controllers
+{
+    public class AuthController : ApiController
+    {
+        public IHttpActionResult GetName()
+        {
+            return Ok("Saulius");
+        }
+
+        //[HttpGet]
+        //[Authorize]
+        //[Route("api/send/{phone:long}/{message}")]
+        //public IHttpActionResult Send(long phone, string message)
+        //{
+        //    var messagingService = new MessagingService("sm.euro@gmail.com", "");
+        //    var result = messagingService.SendMessage(new SmsMessage($"+{phone}", message, "EX0235200"));
+
+        //    return Ok(result);
+        //}
+
+        [Authorize]
+        [Route("api/surname")]
+        public IHttpActionResult GetSurname()
+        {
+            return Ok("Hey hey ");
+        }
+
+        [HttpGet]
+        [Route("api/nogo")]
+        public IHttpActionResult GetNoGo()
+        {
+            return Ok("Not good. Try again");
+        }
+
+        [HttpGet]
+        [Route("api/login")]
+        public HttpResponseMessage Login()
+        {
+            var properties = new AuthenticationProperties() { RedirectUri = "api/surname" };
+            Request.GetOwinContext().Authentication.Challenge(properties, "Google");
+
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+            response.RequestMessage = Request;
+            return response;
+        }
+    }
+}
